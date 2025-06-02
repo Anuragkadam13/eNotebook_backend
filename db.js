b50f541643a51365a8d69b5c223e38b9c606dd59
Anurag Kadam <kadamanurag819@gmail.com>
2025-06-02 18:20:03 +0530

    db fix

--- db.js
+++ db.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const connectToMongo = () => {
-  mongoose.connect(process.env.MONGO_URI);
-  console.log("Connected to Mongo successfully");
+  // mongoose.connect(process.env.MONGO_URI);
+  // console.log("Connected to Mongo successfully");
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Connected to MongoDB successfully");
+    })
+    .catch((err) => {
+      console.error("Error connecting to MongoDB:", err);
+    });
 };
 
 module.exports = connectToMongo;
