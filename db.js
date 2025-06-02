4ddd75c0d11bdb044ad22ca5f62933be1b15916b
Anurag Kadam <kadamanurag819@gmail.com>
2025-06-02 19:04:28 +0530

    db fix

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const connectToMongo = () => {
     process.env.MONGO_URI
   );
 
-  mongoose
+  return mongoose
     .connect(process.env.MONGO_URI, {
       useNewUrlParser: true, // Add these options
       useUnifiedTopology: true,
